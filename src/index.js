e5c746fd7a1aa647b229019d7c22f543a45f3417
Oluyemi Paul <oluola96@gmail,com>
2022-09-21 00:10:06 +0100

    Feat: add Store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import store from './redux/configureStore';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,12 @@
-import { combinReducers } from 'redux';
+import { combinReducers, createStore } from 'redux';
 
 import addBookReducer from './Books/book';
 
 const rootReducer = combinReducers({
     addBook: addBookReducer,
-})
+});
+
+const store = createStore( rootReducer, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() );
+
+    export default store;
