a248ea35527df83774a1cdf89776258f681774e2
Oluyemi Paul <oluola96@gmail,com>
2022-09-21 05:48:38 +0100

    Fix: resolve err

--- src/redux/Books/book.js
+++ src/redux/Books/book.js
@@ -3,40 +3,41 @@ const ADD_BOOK = 'bookLib/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookLib/books/REMOVE_BOOK';
 
 // Create Action Creator
-const addBook = (book) => ({
+export const addBook = (book) => ({
   type: ADD_BOOK,
   book,
 });
-const removeBook = (isbn) => ({
+
+export const removeBook = (isbn) => ({
   type: REMOVE_BOOK,
   isbn,
 });
 
 const initialState = [
-  { 
-    isbn: 1234, 
-    title: 'Learn Python', 
-    author: 'John Matt', 
+  {
+isbn: 1234,
+title: 'Learn Python',
+author: 'John Matt',
 },
-  { 
-    isbn: 1453, 
-    title: 'Learn Java', 
-    author: 'John Doe', 
+  {
+isbn: 1453,
+title: 'Learn Java',
+author: 'John Doe',
 },
 ];
 
-const addBookReducer = (state = initialState, action ={}) => {
+const addBookReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_BOOK:
-      return {
+       case ADD_BOOK:
+         return {
         ...state,
-     book: action.book,
+        book: action.book,
       };
-    case REMOVE_BOOK:
-      return state.filter((isbn) => (isbn !== action.book.isbn));
-    default:
-      return state;
-  }
+        case REMOVE_BOOK:
+          return state.filter((isbn) => (isbn !== action.book.isbn));
+        default:
+          return state;
+    }
 };
 
 export default addBookReducer;
