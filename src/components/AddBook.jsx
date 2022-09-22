70e0675d24bc9e19ab968c564524a5b8c96cfef3
Oluyemi Paul <oluola96@gmail,com>
2022-09-22 13:42:02 +0100

    Feat: connect store to Books component

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -2,26 +2,27 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../redux/Books/book';
 
-const Book = () => {
+const AddBook = () => {
   const dispatch = useDispatch();
-  const submitHandler = (e) => {
-      e.preventDefault();
-      if(!e.target.element[2]) {
-        dispatch(addBook(e.target.element[1].value, 'Anonymous'))
-      } else {
-        dispatch(add(e.target.element[1].value, e.target.element[2].value))
-      }
 
-  }
-  return(
-  <form className="addBook" onSubmit={submitHandler}>
-    <h2>ADD NEW BOOK</h2>
-    <input type="number" name="isbn" placeholder="ISBN" />
-    <input type="text" name="title" placeholder="Book Title" />
-    <input type="text" name="author" placeholder="Book Author" />
-    <button className="addBtn" type="submit">Add Books</button>
-  </form>
-  )
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (!e.target.elements[1].value) {
+      dispatch(addBook(e.target.elements[0].value, 'Anonymous'));
+    } else {
+      dispatch(addBook(e.target.elements[0].value, e.target.elements[1].value));
+    }
+  };
+
+  return (
+    <form className="addBook" onSubmit={submitHandler}>
+      <h2>ADD NEW BOOK</h2>
+      <input type="text" name="title" placeholder="Book Title" required />
+      <input type="text" name="author" placeholder="Book Author" />
+      <button className="addBtn" type="submit">Add Books</button>
+    </form>
+  );
 };
 
-export default Book;
+export default AddBook;
