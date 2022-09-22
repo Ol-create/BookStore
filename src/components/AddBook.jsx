611f7943826631ebc8c0de40df2dbeebef34cc24
Oluyemi Paul <oluola96@gmail,com>
2022-09-22 11:29:41 +0100

    Feat: Add addBook dispatch action to component

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/Books/book';
 
-const AddBook = () => (
-  <form className="addBook">
+const Book = () => {
+  const dispatch = useDispatch();
+  const submitHandler = (e) => {
+      e.preventDefault();
+      if(!e.target.element[2]) {
+        dispatch(addBook(e.target.element[1].value, 'Anonymous'))
+      } else {
+        dispatch(add(e.target.element[1].value, e.target.element[2].value))
+      }
+
+  }
+  return(
+  <form className="addBook" onSubmit={submitHandler}>
     <h2>ADD NEW BOOK</h2>
     <input type="number" name="isbn" placeholder="ISBN" />
     <input type="text" name="title" placeholder="Book Title" />
     <input type="text" name="author" placeholder="Book Author" />
     <button className="addBtn" type="submit">Add Books</button>
   </form>
-);
+  )
+};
 
-export default AddBook;
+export default Book;
