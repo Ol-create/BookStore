62cb250c6483c47de683c027ab7f769b7aeaf216
Oluyemi Paul <oluola96@gmail,com>
2022-09-23 17:16:49 +0100

    Fix: linters err

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
-import reducerCategories from './categories/categories';
+import reducerCategories from './Categories/categories';
 import reducerBook from './books/books';
 
 const reducer = combineReducers({ Books: reducerBook, Categories: reducerCategories });
