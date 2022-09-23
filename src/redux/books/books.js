fa2cbc27e22919366b03266432c1aa485aa7362f
Oluyemi Paul <oluola96@gmail,com>
2022-09-23 15:47:57 +0100

    Feat: Add api data

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -4,33 +4,40 @@ import { v4 as uuidv4 } from 'uuid';
 const ADD_NEW_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
-const defualtBooks = [
-  {
-    id: uuidv4(),
-    title: 'A Place Called Home',
-    author: 'Preeti Shenoy',
-  },
-  {
-    id: uuidv4(),
-    title: 'Leaders, Politicians, Citizens',
-    author: 'Rasheed Kidwai ',
-  },
-  {
-    id: uuidv4(),
-    title: 'The Maverick Effect',
-    author: 'Harish Mehta',
-  },
-];
+const defualtBooks = [];
+
+export const getBooksAPI = (books) => {
+  const APIBooks = Object.entries(books).map(([key, value]) => ({ ...value[0], id: key }));
+  return {
+    type: GET_BOOKS_API,
+    payload: APIBooks,
+  };
+}
+
+export const bookFromAPI = () => async (dispatch) => {
+  const response = await ApiServices.getBooks();
+  dispatch(getBooksAPI(response));
+};
 
 export const createBook = (newBook) => ({
   type: ADD_NEW_BOOK,
   payload: newBook,
 });
 
+export const addNewBooks = (newBook) => async (dispatch) => {
+  await ApiServices.addBook(newBook);
+  dispatch(addNewBook({ ...newBook, id: newBook.item_id }));
+};
+
 export const removeBook = (bookId) => ({
   type: REMOVE_BOOK,
   payload: bookId,
 });
+
+export const removeBooks = (id) => async (dispatch) => {
+  await ApiServices.removeBook(id);
+  dispatch(removeBook(id));
+};
 
 const bookReducer = (initialState = defualtBooks, action) => {
   switch (action.type) {
